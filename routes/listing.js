1cde8ac7d757473096c6c040780e9560f6d84e49
Vinuta2215 <119428186+Vinuta2215@users.noreply.github.com>
2025-04-07 19:57:10 +0530

    Add files via upload

---
+++ routes/listing.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync.js");
+const Listing = require("../models/listing.js"); 
+const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");
+
+const listingController = require("../controllers/listings.js");
+
+const multer = require("multer"); 
+const {storage} = require("../cloudConfig.js");
+const upload = multer({ storage }); 
+
+
+
+router          
+    .route("/")
+    .get( wrapAsync (listingController.index)) //Index Route 
+    .post(   //create route
+        isLoggedIn,
+        upload.single("listing[image]"),   
+        validateListing,  
+        wrapAsync(listingController.createListing)
+    );
+   
+
+    
+//New Route
+router.get("/new", isLoggedIn, listingController.renderNewForm );
+router.get("/filter", wrapAsync(listingController.filterListingsByCategory));
+// Add this to routes/listing.js after other routes
+router.get("/search", wrapAsync(listingController.searchListings));
+
+
+
+router
+    .route("/:id")
+    .get( wrapAsync (listingController.showListing))  //Show route
+    .put(  //update route
+        isLoggedIn,
+        isOwner,
+        upload.single("listing[image]"),   
+        validateListing, 
+        wrapAsync(listingController.updateListing)
+    )
+    .delete(  //delete route
+        isLoggedIn,  
+        isOwner,
+        wrapAsync(listingController.destroyListing)
+    );
+    
+
+
+//Edit Route
+router.get("/:id/edit", 
+    isLoggedIn,
+    isOwner,
+    wrapAsync(listingController.renderEditForm));
+
+
+
+module.exports = router;
